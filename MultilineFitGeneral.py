c3a17a89f55160f328abe1ed50a4358a02af106c
Mário B. Amaro <86668474+mario-b-amaro@users.noreply.github.com>
2023-04-15 16:02:35 +0200

    Update MultilineFitGeneral.py

--- MultilineFitGeneral.py
+++ MultilineFitGeneral.py
@@ -1,3 +1,5 @@
+# THIS SCRIPT IS FITTING log(|RF(R)|^2). ADAPT AS NEEDED
+
 import numpy as np
 import math as m
 import matplotlib.pyplot as plt
@@ -7,25 +9,27 @@ begg=time.time() # UNIX TIME BEGINNING OF RUN
 
 # ------ DATA IMPORT ------
 
-k=np.loadtxt('Data.txt', skiprows=1, usecols=(0,1,2,3))
+k=np.loadtxt('Data.txt', skiprows=1, usecols=(0,1,2,3,4))
 
 xlist=[] # List for x_i (rho')
 ylist=[] # List for y_i (log(|RF(R)|^2))
-elist=[] # List for experimental errors
+y_ulist=[] # List for y_i upper bound
+y_llist=[] # List for y_i lower bound
 
 for i in range(len(k)):
     r=m.sqrt(k[i][1]*(k[i][0]**(1/3)+1)) 
     xlist.append(r)
     ylist.append(m.log10(k[i][2]))
-    elist.append(k[i][3])
+    y_ulist.append(k[i][3])
+    y_llist.append(k[i][4])
     
 # ------ DEFINING N AND TEST INTERVAL ------
 
 # Define an interval and step for each parameter a, b, c
 
-alist=np.linspace(0,-0.1,10)
-blist=np.linspace(-3.5,0,10)
-clist=np.linspace(-1,-2.5,8)
+alist=np.linspace(0.086,0.093,11)
+blist=np.linspace(-2.85,-2.77,11)
+clist=np.linspace(-1.25,-1.38,11)
 
 nn=2 # Number of lines to fit the data to
 dlist=np.identity(nn) # Identity matrix (delta)
@@ -93,14 +97,26 @@ plt.plot(err_sum_list,'k')
 
 # ------ CALCULATING ERROR FILE ------
 
-k=np.loadtxt('Data.txt', skiprows=1, usecols=(0,1,2,3,5))
-k2=np.genfromtxt('Data.txt',dtype='str', usecols=4)
+k=np.loadtxt('Data.txt', skiprows=1, usecols=(0,1,2,3,4,6,7))
 f= open('Data_w_Errors_RF(R)2.txt',"w+")
 
 modelpred=[linesopt[i]*(combo[0]*xlist[i]+combo[1])+combo[2] for i in range(len(xlist))] # Model prediction for log(|RF(R)|^2)
 modelerr_log=[ylist[i]-modelpred[i] for i in range(len(ylist))] # Error_log = log(|RF(R)|^2) - Model_log(|RF(R)|^2)
 modelerr=[10**ylist[i]-10**modelpred[i] for i in range(len(ylist))] # Error = |RF(R)|^2 - Model_|RF(R)|^2
-test_log=[abs(modelerr[i])<k[i][3] for i in range(len(modelerr))] # Test if |Error|<|Expr_err|
+test_log=[(y_ulist[i]>10**modelpred[i] and y_llist[i]<10**modelpred[i]) for i in range(len(modelpred))] # Test if |RF(R)|^2_l < Model < |RF(R)|^2_u
+up_err=[y_ulist[i]-10**ylist[i] for i in range(len(ylist))] 
+low_err=[10**ylist[i]-y_llist[i] for i in range(len(ylist))]
+
+# ------ OBTAINING GROUND ERROR PLOT ------
+
+xrange=np.linspace(0,len(modelerr),100)
+plt.figure(constrained_layout=True)
+plt.plot(xrange,0*xrange,'--r')
+plt.scatter(range(len(modelerr)),modelerr,s=75,c='k',marker='.')
+plt.errorbar(range(len(modelerr)),modelerr,yerr=[low_err,up_err],c='k',fmt='.')
+plt.ylabel(r'$\mathregular{|RF(R)|^2-10^{(k-1)(\alpha x_{i}+\beta)-o}}$')
+plt.xlabel('Data Point (i)')
+plt.savefig('abserrors', dpi=800)
 
 # ------ FORMATTING ERROR FILE ------
 
@@ -109,24 +125,25 @@ for i in range(len(test_log)):
         test_log[i]=test_log[i]
     else:
         test_log[i]='x'
+        
+for i in range(len(modelerr_log)):
+    if modelerr_log[i]>0:
+        modelerr_log[i]=str(format(modelerr_log[i],'.5f'))
+    else:
+        modelerr_log[i]=str(format(modelerr_log[i],'.4f'))
+        
+for i in range(len(modelerr)):
+    if modelerr[i]>0:
+        modelerr[i]=str(format(modelerr[i],'.5f'))
+    else:
+        modelerr[i]=str(format(modelerr[i],'.4f'))
 
-f.write('A Z |RF(R)|^2 Expr_err F(R)/PDU || Model_log(|RF(R)|^2) Error_log Model_|RF(R)|^2 Error |Error|<|Expr_err| \n')
+f.write('A Z |RF(R)|^2 |RF(R)|^2_u |RF(R)|^2_l F(R)/PDU PDU || Model_log(|RF(R)|^2) Error_log Model_|RF(R)|^2 Error |RF(R)|^2_l < Model < |RF(R)|^2_u \n')
 
 for i in range(len(k)):
-    f.write(str(int(k[i][0]))+' '+str(int(k[i][1]))+' '+str(format(k[i][2],'.6f'))+' '+str(format(k[i][3],'.3f'))+' '+str(format(k[i][4],'.4f'))+' || '+str(format(modelpred[i],'.4f'))+' '+str(format(modelerr_log[i],'.4f'))+' '+str(format(10**modelpred[i],'.4f'))+' '+str(format(modelerr[i],'.4f'))+' '+str(test_log[i])+'\n')
+    f.write(str(int(k[i][0]))+' '+str(int(k[i][1]))+' '+str(format(k[i][2],'.6f'))+' '+str(format(k[i][3],'.5f'))+' '+str(format(k[i][4],'.5f'))+' '+str(format(k[i][5],'.4f'))+' '+str(format(k[i][6],'.4f'))+' || '+str(format(modelpred[i],'.4f'))+' '+modelerr_log[i]+' '+str(format(10**modelpred[i],'.4f'))+' '+modelerr[i]+' '+str(test_log[i])+'\n')
     
 f.close()
-
-# ------ OBTAINING GROUND ERROR ------
-
-xrange=np.linspace(0,len(modelerr),100)
-plt.figure(constrained_layout=True)
-plt.plot(xrange,0*xrange,'--r')
-plt.scatter(range(len(modelerr)),modelerr,s=75,c='k',marker='.')
-plt.errorbar(range(len(modelerr)),modelerr,yerr=elist,c='k',fmt='.')
-plt.ylabel(r'$\mathregular{10^{y_{i}}-10^{(k-1)(\alpha x_{i}+\beta)-o}}$')
-plt.xlabel('Data Point (i)')
-plt.savefig('abserrors', dpi=800)
 
 fin=time.time() # UNIX TIME END OF RUN
 
